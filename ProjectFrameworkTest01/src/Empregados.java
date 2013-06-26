c432bcbbef9a2187a65b2f0062a9078c9a701725
rdallagnol <rodrigodall7@gmail.com>
2013-06-25 21:30:06 -0300

    Teste

---
+++ ProjectFrameworkTest01/src/Empregados.java
@@ -0,0 +1,33 @@
+public class Empregados {
+
+	private String nome;
+
+	private int salMensal;
+
+	private int idade;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public float getSalMensal() {
+		return salMensal;
+	}
+
+	public void setSalMensal(int salMensal) {
+		this.salMensal = salMensal;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+}
